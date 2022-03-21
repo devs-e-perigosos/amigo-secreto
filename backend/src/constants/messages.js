56a17554be4d0a17adf83eebbeacb5458b681d75
ivesfg1 <wigzfurtado@gmail.com>
2022-03-21 19:11:24 -0300

    feat/fix: refatorado codigo de getAmigoById com find e implementado rota de deletar amigo

--- backend/src/constants/messages.js
+++ backend/src/constants/messages.js
@@ -4,6 +4,7 @@ export const MESSAGES = {
   JA_EXISTE_UM_AMIGO_COM_ESSE_NOME: 'Já existe um amigo com esse nome!',
   JA_EXISTE_UM_AMIGO_COM_ESSE_EMAIL: 'Já existe um amigo com esse e-mail!',
   AMIGO_ADICIONADO_COM_SUCESSO: 'Amigo adicionado com sucesso!',
+  AMIGO_REMOVIDO_COM_SUCESSO: 'Amigo removido com sucesso!',
   NAO_EXISTE_UM_AMIGO_COM_ESSE_ID: 'Não existe um amigo com esse Id!',
   AMIGO_EDITADO_COM_SUCESSO: 'Amigo editado com sucesso!',
   ESSE_AMIGO_JA_POSSUI_ESSES_DADOS: 'Esse amigo já possui esses dados!',
