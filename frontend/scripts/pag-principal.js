6f976235886bc575cfdc2ac82ff6e6708784d338
João Alfredo Alves <68473607+JoaoAlfredoAlves@users.noreply.github.com>
2022-03-24 15:46:42 -0300

    Editando tela de Confirmação de Remoção de Amigo

--- frontend/scripts/pag-principal.js
+++ frontend/scripts/pag-principal.js
@@ -6,151 +6,163 @@ const botaoSorteio = document.querySelector("#btn-sorteio");
 const botoes = document.querySelector(".btns");
 let dados;
 async function obterAmigos() {
-  const response = await fetch("http://localhost:3000/amigos/all");
-  const loader = document.querySelector(".lds-ring");
-  loader.style.display = "none";
-  if (response.status === 204) {
-    img.style.display = "block";
-    botaoCadastro.style.display = "flex";
-  } else if (response.status === 200) {
-    dados = await response.json();
-    obs.style.display = "flex";
-    dados.forEach(({ nome, email, id }) => {
-      criarCard(nome, email, id);
-    });
-    botoes.style.display = "flex";
-    botaoCadastro.style.display = "none";
-    if (dados.length < 3) {
-      botaoSorteio.style.display = "none";
-    } else {
-      botaoSorteio.style.display = "inline-block";
-    }
-  }
+	const response = await fetch("http://localhost:3000/amigos/all");
+	const loader = document.querySelector(".lds-ring");
+	loader.style.display = "none";
+	if (response.status === 204) {
+		img.style.display = "block";
+		botaoCadastro.style.display = "flex";
+	} else if (response.status === 200) {
+		dados = await response.json();
+		obs.style.display = "flex";
+		dados.forEach(({ nome, email, id }) => {
+			criarCard(nome, email, id);
+		});
+		botoes.style.display = "flex";
+		botaoCadastro.style.display = "none";
+		if (dados.length < 3) {
+			botaoSorteio.style.display = "none";
+		} else {
+			botaoSorteio.style.display = "inline-block";
+		}
+	}
 }
 
 function criarCard(nome, email, id) {
-  const card = document.createElement("div");
-  const buttons = document.createElement("span");
-  const data = document.createElement("span");
-  const nomePessoa = document.createElement("p");
-  const emailPessoa = document.createElement("p");
+	const card = document.createElement("div");
+	const buttons = document.createElement("span");
+	const data = document.createElement("span");
+	const nomePessoa = document.createElement("p");
+	const emailPessoa = document.createElement("p");
 
-  const img1 = document.createElement("img");
-  img1.src = "../img/editButton.svg";
+	const img1 = document.createElement("img");
+	img1.src = "../img/editButton.svg";
 
-  const img2 = document.createElement("img");
-  img2.src = "../img/deleteButton.svg";
+	const img2 = document.createElement("img");
+	img2.src = "../img/deleteButton.svg";
 
-  card.setAttribute("class", "card");
-  buttons.setAttribute("class", "buttons");
+	card.classList.add("card");
+	buttons.classList.add("buttons");
+	data.classList.add("data");
 
-  data.setAttribute("class", "data");
+	nomePessoa.classList.add("nome");
+	nomePessoa.innerText = `${nome}`;
 
-  nomePessoa.setAttribute("class", "nome");
-  nomePessoa.innerText = `${nome}`;
+	emailPessoa.classList.add("email");
+	emailPessoa.innerText = `${email}`;
 
-  emailPessoa.setAttribute("class", "email");
-  emailPessoa.innerText = `${email}`;
+	data.appendChild(nomePessoa);
+	data.appendChild(emailPessoa);
 
-  data.appendChild(nomePessoa);
-  data.appendChild(emailPessoa);
+	buttons.appendChild(img1);
+	buttons.appendChild(img2);
 
-  buttons.appendChild(img1);
-  buttons.appendChild(img2);
+	img1.addEventListener("click", (_) => {
+		document.location.href = `../pages/tela-edicao-cadastro.html?id=${id}`;
+	});
 
-  img1.addEventListener("click", (_) => {
-    document.location.href = `../pages/tela-edicao-cadastro.html?id=${id}`;
-  });
+	img2.addEventListener("click", (_) => confirmarDelete(nome, id));
 
-  img2.addEventListener("click", (_) => confirmarDelete(nome, id));
+	card.appendChild(buttons);
+	card.appendChild(data);
 
-  card.appendChild(buttons);
-  card.appendChild(data);
-
-  participantes.appendChild(card);
+	participantes.appendChild(card);
 }
 /**/
 function confirmarDelete(nome, id) {
-  const main = document.querySelector("main");
-  const body = document.querySelector("body");
-  main.style.display = "none";
+	const main = document.querySelector("main");
+	const body = document.querySelector("body");
+	main.style.display = "none";
 
-  const frases = document.createElement("div");
-  const mensagemExclusao = document.createElement("p");
-  const mensagemExclusaoPessoa = document.createElement("p");
+	const pagRemove = document.createElement("div");
+	body.appendChild(pagRemove);
+	pagRemove.classList.add("pag-remove");
 
-  const buttons = document.createElement("div");
-  const buttonSim = document.createElement("button");
-  const buttonCancelar = document.createElement("button");
+	const frases = document.createElement("div");
 
-  buttonSim.addEventListener("click", async (_) => {
-    const config = {
-      method: "DELETE",
-    };
+	const mensagemExclusao = document.createElement("p");
+	const mensagemExclusaoPessoa = document.createElement("p");
 
-    await fetch(`http://localhost:3000/amigos/${id}`, config);
+	const buttons = document.createElement("div");
 
-    dados = dados.filter((dado) => {
-      return dado.id != id;
-    });
-    participantes.innerHTML = "";
+	const buttonSim = document.createElement("button");
+	const buttonCancelar = document.createElement("button");
 
-    if (dados.length === 0) {
-      obs.style.display = "none";
-      participantes.style.display = "none";
-      botoes.style.display = "none";
-      img.style.display = "block";
-      botaoCadastro.style.display = "flex";
-    } else {
-      dados.forEach(({ nome, email, id }) => {
-        criarCard(nome, email, id);
-      });
-      botoes.style.display = "flex";
-      botaoCadastro.style.display = "none";
-      if (dados.length < 3) {
-        botaoSorteio.style.display = "none";
-      } else {
-        botaoSorteio.style.display = "inline-block";
-      }
-    }
+	frases.classList.add("frases");
+	mensagemExclusao.classList.add("mensagem-exclusao");
+	mensagemExclusaoPessoa.classList.add("mensagem-exclusao-pessoa");
+	buttons.classList.add("buttonsSimNao");
+	buttonSim.classList.add("button-sim");
+	buttonCancelar.classList.add("button-cancelar");
 
-    frases.style.display = "none";
-    buttons.style.display = "none";
-    main.style.display = "flex";
-  });
+	buttonSim.addEventListener("click", async (_) => {
+		const config = {
+			method: "DELETE",
+		};
 
-  buttonCancelar.addEventListener("click", (_) => {
-    frases.style.display = "none";
-    buttons.style.display = "none";
-    main.style.display = "flex";
-  });
+		await fetch(`http://localhost:3000/amigos/${id}`, config);
 
-  buttonSim.innerText = "Sim";
-  buttonCancelar.innerText = "Não";
+		dados = dados.filter((dado) => {
+			return dado.id != id;
+		});
+		participantes.innerHTML = "";
 
-  mensagemExclusao.innerText = "Deseja realmente excluir";
-  mensagemExclusaoPessoa.innerText = `${nome} ?`;
+		if (dados.length === 0) {
+			obs.style.display = "none";
+			participantes.style.display = "none";
+			botoes.style.display = "none";
+			img.style.display = "block";
+			botaoCadastro.style.display = "flex";
+		} else {
+			dados.forEach(({ nome, email, id }) => {
+				criarCard(nome, email, id);
+			});
+			botoes.style.display = "flex";
+			botaoCadastro.style.display = "none";
+			if (dados.length < 3) {
+				botaoSorteio.style.display = "none";
+			} else {
+				botaoSorteio.style.display = "inline-block";
+			}
+		}
 
-  frases.appendChild(mensagemExclusao);
-  frases.appendChild(mensagemExclusaoPessoa);
+		frases.style.display = "none";
+		buttons.style.display = "none";
+		main.style.display = "flex";
+	});
 
-  buttons.appendChild(buttonSim);
-  buttons.appendChild(buttonCancelar);
+	buttonCancelar.addEventListener("click", (_) => {
+		frases.style.display = "none";
+		buttons.style.display = "none";
+		main.style.display = "flex";
+	});
 
-  body.appendChild(frases);
-  body.appendChild(buttons);
+	buttonSim.innerText = "Sim";
+	buttonCancelar.innerText = "Não";
+
+	mensagemExclusao.innerText = "Deseja realmente excluir";
+	mensagemExclusaoPessoa.innerText = `${nome} ?`;
+
+	frases.appendChild(mensagemExclusao);
+	frases.appendChild(mensagemExclusaoPessoa);
+
+	buttons.appendChild(buttonSim);
+	buttons.appendChild(buttonCancelar);
+
+	pagRemove.appendChild(frases);
+	pagRemove.appendChild(buttons);
 }
 
 function adicionarListenerBotaoSorteio() {
-  const config = {
-    method: "PUT",
-  };
+	const config = {
+		method: "PUT",
+	};
 
-  botaoSorteio.addEventListener("click", async (_) => {
-    //Mudar para tela de loading
-    await fetch("http://localhost:3000/amigos/sorteio", config);
-    //Mudar tela para "Sorteio realizado com sucesso!"
-  });
+	botaoSorteio.addEventListener("click", async (_) => {
+		//Mudar para tela de loading
+		await fetch("http://localhost:3000/amigos/sorteio", config);
+		//Mudar tela para "Sorteio realizado com sucesso!"
+	});
 }
 
 obterAmigos();
